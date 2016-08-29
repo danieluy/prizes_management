7fec62045377b0f52a83b294ee013a19ece53c96
danieluy <danielsosa.dev@gmail.com>
2016-08-29 10:59:27 -0300

    Working on users save method First version complete, have to start testing it

--- my_modules/db_users.js
+++ my_modules/db_users.js
@@ -1,46 +1,76 @@
 
 "use strict";
-const fs = require('fs');
-const mongodb = require('mongodb');
-const mongo = mongodb.MongoClient;
-const config = require('../config.json');
-const db_ip = config.database.ip;
-const db_port = config.database.port;
-const db_name = config.database.name;
-const url = 'mongodb://' + db_ip + ':' + db_port + '/' + db_name;
-const security = require('./security.js');
-const ObjectID = require('mongodb').ObjectID;
-let current_userNames = [];
+const fs = require('fs'),
+      mongodb = require('mongodb'),
+      mongo = mongodb.MongoClient,
+      config = require('../config.json'),
+      db_ip = config.database.ip,
+      db_port = config.database.port,
+      db_name = config.database.name,
+      url = 'mongodb://' + db_ip + ':' + db_port + '/' + db_name,
+      security = require('./security.js'),
+      ObjectID = require('mongodb').ObjectID;
+let g_current_userNames = [];
 
-const init = () => {
-  // pre-fetch the user names so that it can use them to check if the new user name already exists
-  let interval = setInterval(()=>{
-    findAll()
-    .then((results) => {
-      if(results.length)
-        clearInterval(interval);
-      current_userNames = results.map((result) => result.getUserName());
-      console.log('current_userNames');
-      console.log(current_userNames);
-    })
-    .catch((err)=>{console.error('ERROR Can\'t fetch the current user names - db_users module - Returned ERROR: ' + err)});
-  },1000)
-}
+// const init = () => {
+//   // pre-fetch the user names so that it can use them to check if the new user name already exists
+//   let interval = setInterval(()=>{
+//     findAll()
+//     .then((results) => {
+//       if(results.length)
+//         clearInterval(interval);
+//       g_current_userNames = results.map((result) => result.getUserName());
+//     })
+//     .catch((err) => {console.error('ERROR Can\'t fetch the current user names - db_users module - Returned ERROR: ' + err)});
+//   },500)
+// }
 
 const User = function(user_info){
-  console.log('current_userNames.indexOf(user_info.userName) = ', current_userNames.indexOf(user_info.userName));
   if(!user_info.userName || !user_info.password || !user_info.role)
     throw 'ERROR: To create a new user, "userName", "password" and "role", must be provided';
   if(user_info.role.toLowerCase() !== 'admin' && user_info.role.toLowerCase() !== 'user')
     throw 'ERROR: The new user\'s "role" can only be "admin" or "user"';
-  if(current_userNames.indexOf(user_info.userName.toLowerCase()) >= 0)
-    throw 'ERROR: The user name "' + user_info.userName + '" is already in use';
+
+  // Properties
   let id = user_info.id ? ObjectID(user_info.id) : null,
   userName = user_info.userName.toLowerCase(),
   password = user_info.password,
   role = user_info.role.toLowerCase(),
   email = user_info.email ? user_info.email.toLowerCase() : null,
   set_date = user_info.set_date || Date.now();
+
+  // Methods
+  const save = () => {
+    return new Promise((resolve, reject) => {
+      // Check if the userName already exists
+      findUserName(userName)
+      .then((result) => {
+        if(result) return reject('The user name "' + userName + '" is already been taken');
+        mongo.connect(url, function(err, db){
+          if(err){
+            db.close();
+            return reject('ERR_DB - Unable to connect to the database - db_users module - Returned ERROR: ' + err);
+          }
+          else{
+            var users = db.collection('users');
+            users.insert({
+              'userName': userName,
+              'password': password,
+              'role': role,
+              'email': email,
+              'set_date': set_date
+            }, (WriteResult) => {
+              db.close();
+              return resolve(WriteResult);
+            });
+          }
+        });
+      })
+
+    });
+
+  }
+
   return {
     getId: () => id.valueOf(),
     getUserName: () => userName,
@@ -49,7 +79,8 @@ const User = function(user_info){
     setEmail: (_email) => {email = _email},
     getRole: () => role,
     setRole: (_role) => {role = _role},
-    getSet_date: () => set_date
+    getSet_date: () => set_date,
+    save: save
   }
 }
 
@@ -93,16 +124,18 @@ const findUserName = (_userName) => {
         users.find({userName: _userName}).toArray((err, array) => {
           db.close();
           if(err) return reject('ERR_DB - Unable to fetch prizes data - db_users module - Returned ERROR: ' + err);
-          return resolve(array.map((r) => {
-            return new User({
-              id: r._id,
-              userName: r.userName,
-              password: r.password,
-              email: r.email,
-              role: r.role,
-              set_date: r.set_date
-            });
-          }));
+          return resolve((() => {
+            if(array.length)
+              return new User({
+                id: array[0]._id,
+                userName: array[0].userName,
+                password: array[0].password,
+                email: array[0].email,
+                role: array[0].role,
+                set_date: array[0].set_date
+              });
+            return null;
+          })());
         });
       }
     });
@@ -110,7 +143,7 @@ const findUserName = (_userName) => {
 }
 
 module.exports = {
-  init: init,
+  // init: init,
   User: User,
   findUserName: findUserName
 };
--- testing.js
+++ testing.js
@@ -5,7 +5,7 @@
 
 // db_users  ///////////////////////////////////////////////////////////////////
 const db_users = require('./my_modules/db_users.js');
-db_users.init();
+// db_users.init();
 const User = db_users.User;
 
 function newUser(){
@@ -19,13 +19,30 @@ function newUser(){
 	})
 	console.log(new_user.getUserName());
 	console.log(new_user);
+	return new_user;
 }
 // newUser();
-setTimeout(newUser, 1100);
+// setTimeout(newUser, 1100);
 
 function findUserByName(){
-  db_users.findUserName('franco')
-  .then((results) => {console.log(results)})
+	let candidate = 'Tyler Durden';
+  db_users.findUserName(candidate)
+  .then((result) => {
+		if(result)
+			console.log('The user name "'+candidate+'" is already taken');
+		else
+			console.log('The user name "'+candidate+'" is free to use');
+	})
   .catch((err) => console.error(err))
 }
 // findUserByName();
+
+function saveUser(){
+	newUser()
+	.save()
+	.then((WriteResult) => {console.log(WriteResult)})
+	.catch((err) => {
+		console.error(err);
+	})
+}
+saveUser()
